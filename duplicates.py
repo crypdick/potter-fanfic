b2779811f05343f4d43d79b152306e88dece5c4f
Richard Decal <richard.decal@gmail.com>
2017-09-27 00:03:21 -0400

    simplified hash func to just ord everything, added comments, fixed read_csv misspelling, simplified indexing notation of hash table generation, simplified indexing notation of deduplication

--- duplicates.py
+++ duplicates.py
@@ -1,89 +1,72 @@
 import time
 import pandas as pd
+#
+# with open('data/stories_orig.csv', 'r') as file:
+#     stories_a = file.read()
 
-with open('data/stories_orig.csv', 'r') as file:
-	stories_a = file.read()
-
-stories_b = stories_a
-
-#5A
-#One program sorts the rows of stories_orig.csv and traverses rows in sorted order.
+# FIRST IMPLEMENTATION: sorts the rows traverses rows in sorted order.
 
 stories_a = pd.read_csv('data/stories_orig.csv')
 
 start_time = time.time()
 
-stories_a = stories_a.sort_values(by='URL')
+stories_a = stories_a.sort_values(by='URL')  # backend quicksort
 
 previous_url = ''
 for i in stories_a.index:
-	current_url = stories_a.ix[i, 'URL']
-	if current_url == previous_url:
-		stories_a.drop(i, inplace=True)
-	previous_url = current_url
-
-#save file
+    current_url = stories_a.ix[i, 'URL']
+    if current_url == previous_url:
+        stories_a.drop(i, inplace=True)
+    previous_url = current_url
 
 time_elapsed = time.time() - start_time
 
-print('5A: ' + str(time_elapsed))
+print('Time elapsed 5A - sort + traverse: ' + str(time_elapsed))
 
-# 5A: 39.28861618041992
+# Time elapsed 5A - sort + traverse: 15.055665969848633
 
-#5B
-#The other program uses a hash:
-#Find a way to map each row r to a number h(r) between 0 and N = 2^20-1. 
-#One suggestion (among many): represent published as mmddyy and words as nnn...n, and set h(r) to mmddyynnn...n mod N.
-#Create a python list of N empty lists.
-#Add each row r to the h(r)th list. 
-#Traverse the list of lists. If any list contains more than one row, compare them. 
+# SECOND IMPLEMENTATION: hashing each row r to a number h(r) between 0 and N = 2^20-1
 
-stories_b = pd.read('data/stories_orig.csv')
+stories_b = pd.read_csv('data/stories_orig.csv')
 
-hash_table = [[] for i in range(2**20)]
+hash_table = [[] for i in range(2 ** 20)]
+
 
 def hash_function(date, url):
-	hashed_index = 0
-	if type(date) == type('08-25-1999'):
-		hashed_index = int(date.replace('-', ''))
-	for character in url:
-		hashed_index += ord(character) # multiply by index
-	return hashed_index % (2**20)
+    '''start hash with publish date, then for each character add the ordinal value to the index. finally, find remainder
+    of dividing by 2^20
+    '''
+    hashed_index = 0
+    #if isinstance(date, str):
+    #    hashed_index = int(date.replace('-', ''))  # remove - from date to a void ValueError
+    for character in str(date)+url:
+        hashed_index += ord(character)  # multiply by index
+    return hashed_index % (2 ** 20)
+
 
 start_time = time.time()
 
-for row in stories_b.iterrows():
-	date, url = row[1][['PUBLISHED', 'URL']]
-	hashed_index = hash_function(date, url)
-	hash_table[hashed_index].append(row[1])
+# store data in hash table
+for index, row in stories_b.iterrows():
+    date, url = row[['PUBLISHED', 'URL']]
+    hashed_index = hash_function(date, url)
+    hash_table[hashed_index].append(row.to_dict())  #
 
+print("done hashing ", time.time() - start_time)
 
-for hash_table_index in range(len(hash_table)):
-	if len(hash_table[hash_table_index]) > 1:
-		urls = []
-		for sublist_index in range(len(hash_table[hash_table_index])):
-			try:
-				if hash_table[hash_table_index][sublist_index]['URL'] in urls:
-					del hash_table[hash_table_index][sublist_index]
-				else:
-					urls.append(hash_table[hash_table_index][sublist_index]['URL'])
-			except:
-				print(sublist_index, hash_table[hash_table_index])
-
-#save file
+# deduplicate entries
+for i, hashed_list in enumerate(hash_table):
+    if len(hashed_list) > 1:  # potential dupes
+        urls = []
+        for j, entry in enumerate(hashed_list):
+            url = entry['URL']
+            if url in urls:
+                del hash_table[i][j]
+            else:
+                urls.append(url)
 
 time_elapsed = time.time() - start_time
 
-print('5B: ' + str(time_elapsed))
+print('Time elapsed 5B - hashing: ' + str(time_elapsed))
 # 5B: 239.42458391189575
-
-#5C
-#Which program runs faster? Explain
-
-# 5A is faster, although I am not sure if that should be the case.
-
-# 5A has an initial sort, which is NlogN and then queries through the list to find duplicates, which is N
-
-# 5B populates a hash table O(N) and then runs queries through the table O(N)
-
-# Because of the speed limitations of pandas, this was not a reality.
+#Time elapsed 5B - hashing: 465.03688859939575
